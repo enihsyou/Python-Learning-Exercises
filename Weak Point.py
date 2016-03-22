ac92c19c7e8a1ad4d382f94f6fdfbab4ebc629a1
enihsyou <s1131234@gmail.com>
2016-03-22 23:13:59 +0800

    some exercises

---
+++ Weak Point.py
@@ -0,0 +1,11 @@
+# def weak_point(input):
+#     minr = [sum(add) for add in input]
+#     minc = [sum([data[i] for data in input]) for i in range(len(input[0]))]
+#     return [minr.index(min(minr)), minc.index(min(minc))]
+
+
+def weak_point(matrix):
+    row_sums = list(map(sum, matrix))
+    column_sums = list(map(sum, list(zip(*matrix))))
+    return row_sums.index(min(row_sums)), column_sums.index(min(column_sums))
+weak_point([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
