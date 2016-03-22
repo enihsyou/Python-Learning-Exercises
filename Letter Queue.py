4058bd70adb9546883e97d2583ffad50afa19b6e
enihsyou <s1131234@gmail.com>
2016-03-22 22:50:55 +0800

    some exercises

---
+++ Letter Queue.py
@@ -0,0 +1,20 @@
+def letter_queue(data):
+    fu = []
+    for func in data:
+        fu.append(func.split())
+    dic = {"PUSH": lambda x, y: x.append(y),
+           "POP": lambda x, y: x.pop(y)}
+    queue = []
+    for item in fu:
+        if len(item) == 2:
+            dic[item[0]](queue, item[1])
+        elif queue:
+            dic[item[0]](queue, 0)
+        else: continue
+    return "".join(queue)
+
+
+letter_queue(["PUSH A", "POP", "POP", "PUSH Z", "PUSH D", "PUSH O", "POP", "PUSH T"])
+letter_queue(["POP", "POP"])
+letter_queue(["PUSH H", "PUSH I"])
+letter_queue([])
