77ac356ae59dea9e57531a7d616ac285cbcfd7a2
enihsyou <s1131234@gmail.com>
2016-03-26 23:35:52 +0800

    cowsay

--- What does the cow say.py
+++ What does the cow say.py
@@ -2,53 +2,57 @@ def cowsay(says):
     # Multiple spaces in a row are replaced by one space.
     say = says.split()
 
-    # If line is less than 40 characters, it will fit into one string. The string is quoted in <>.
-    if len(says) < 40:
-        # top and bottom
-        said = "< " + " ".join(say) + " >"
-        top = "_" * (len(said) - 2)
-        bottom = "-" * (len(said) - 2)
-    else:
-        # top and bottom
-        top = "_" * 41
-        bottom = "-" * 41
+    line = []
+    con_line_sum = 0  # 当前行的字数
+    con_line = []  # 当前行的字符
+    while say:  # 持续循环 只要还有
+        pops = say.pop(0)
+        con_line_sum += len(pops)
+        con_line.append(pops)
+        if con_line_sum < 39:  # 还有空间 添加空格
+            con_line_sum += 1
+            continue
 
-        line = []
-        con_line_sum = 0  # 当前行的字数
-        con_line = []  # 当前行的字符
-        while say:  # 持续循环 只要还有
-            pops = say.pop(0)
-            con_line_sum += len(pops)
-            con_line.append(pops)
-            if con_line_sum < 39:  # 还有空间 添加空格
-                con_line_sum += 1
-                continue
-
-            elif con_line_sum == 39:
-                line.append(" ".join(con_line))
-                con_line_sum = 0
-                con_line = []
-
-            else:  # 超过39 不要最后一个
-                say.insert(0, con_line.pop())
-                line.append(" ".join(con_line))
-                con_line_sum = 0
-                con_line = []
-        if con_line:
+        elif con_line_sum == 39:
             line.append(" ".join(con_line))
+            con_line_sum = 0
+            con_line = []
 
-        # now we have each line
-        result_line = []
+        else:  # 超过39 不要最后一个
+            con_pop = con_line.pop()
+            if len(con_pop) <= 39:
+                say.insert(0, con_pop)
+                line.append(" ".join(con_line))
+            else:
+                line.append(con_pop[:39])
+                say.append(con_pop[39:])
+            con_line_sum = 0
+            con_line = []
+    if con_line:
+        line.append(" ".join(con_line))
+
+    # now we have each line
+    result_line = []
+
+    # top and bottom
+    max_len = len(max(line, key = lambda x: len(x)))
+
+    top = "_" * (max_len + 2)
+    bottom = "-" * (max_len + 2)
+
+    if len(line) == 1:
+        result_line.append("< " + " ".join(line) + " >")
+    else:
         for each in range(len(line)):
             if each == 0:
-                result_line.append("/ " + line[each].ljust(39) + " \\")
+                result_line.append("/ " + line[each].ljust(max_len) + " \\")
             elif each == len(line) - 1:
-                result_line.append("\\ " + line[each].ljust(39) + " /")
+                result_line.append("\\ " + line[each].ljust(max_len) + " /")
             else:
-                result_line.append("| " + line[each].ljust(39) + " |")
-        # print(line)
-        said = "\n".join(result_line)
-        # print(said)
+                result_line.append("| " + line[each].ljust(max_len) + " |")
+    # print(line)
+    said = "\n".join(result_line)
+    # print(said)
     print(r'''
  {top}
 {said}
@@ -61,6 +65,8 @@ def cowsay(says):
 '''.format(said = said, top = top, bottom = bottom))
 
 
+cowsay("looooooooooooooooooooooooooooooooooooooooooooooong")
+cowsay("loooooooooooooooooooooooooooooooooooong")
 cowsay("spaces                           inside")
 cowsay('Checkio rulezz')
 cowsay('A longtextwithonlyonespacetofittwolines.')
