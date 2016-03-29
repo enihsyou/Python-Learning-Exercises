3f6b7c3d0a4ecf2622c29bad1ba92e948295526d
enihsyou <s1131234@gmail.com>
2016-03-29 18:40:28 +0800

    Factorial without "eiou" V3.0 a tricky way

--- Univocalic Factaral.py
+++ Univocalic Factaral.py
@@ -1,11 +1,11 @@
 # 1st
 # a_factaral = lambda x: (x > 1 and x * a_factaral(x - 1)) + (x < 2 and 1)
 
-# 2nd
-a_factaral = lambda x: (x and x * a_factaral(x - 1)) + (x < 1 and 1)
+# 2nd 193pt@153rd
+# a_factaral = lambda x: (x and x * a_factaral(x - 1)) + (x < 1 and 1)
 
-# 3rd
-#a_factaral = lambda x: (x and x * a_factaral(x - 1)) + (x < 1 and 1)
+# 3rd a tricky way
+b = a_factaral = lambda x: (x and x * b(x - 1)) + (x < 1 and 1)
 print(a_factaral(2))
 print(a_factaral(10))
 print(a_factaral(5))
