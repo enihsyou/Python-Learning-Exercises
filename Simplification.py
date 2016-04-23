5f254e44e742f00c5b056751f7988cc0d253c915
enihsyou <s1131234@gmail.com>
2016-04-23 15:23:10 +0800

    Done Class x and a Recursive Descent Parser

--- Simplification.py
+++ Simplification.py
@@ -1,8 +1,6 @@
 import re
 import collections
 
-import math
-
 X = r'(?P<X>x)'
 NUM = r'(?P<NUM>\d+)'
 PLUS = r'(?P<PLUS>\+)'
@@ -36,17 +34,21 @@ class X:
         if isinstance(other, int):
             self.power[0] += other
         elif isinstance(other, X):
-            for i in range(len(min(self.power, other.power))):
+            for i in range(len(other.power)):
+                if i >= len(self.power): self.power.append(0)
                 self.power[i] += other.power[i]
-        return self.__repr__()
+        # return self.__repr__()
+        return self
 
     def __sub__(self, other):
         if isinstance(other, int):
             self.power[0] -= other
         elif isinstance(other, X):
-            for i in range(len(min(self.power, other.power))):
+            for i in range(len(other.power)):
+                if i >= len(self.power): self.power.append(0)
                 self.power[i] -= other.power[i]
-        return self.__repr__()
+        # return self.__repr__()
+        return self
 
     def __mul__(self, other):
         if isinstance(other, int):
@@ -61,47 +63,60 @@ class X:
                     if i + j >= len(self.power):
                         self.power.extend([0] * (i + j - len(self.power) + 1))
                     self.power[i + j] += power[i] * power2[j]
+        return self
 
     def __radd__(self, other):
         self.__add__(other)
+        return self
 
     def __rsub__(self, other):
-        self.__sub__(other)
+        self.power = [-x for x in self.power]
+        if isinstance(other, int):
+            self.power[0] += other
+        elif isinstance(other, X):
+            for i in range(len(other.power)):
+                if i >= len(self.power): self.power.append(0)
+                self.power[i] += other.power[i]
+        # return self.__repr__()
+        return self
 
     def __rmul__(self, other):
         self.__mul__(other)
+        return self
 
     def __repr__(self):
         string = ""
         for index, num in enumerate(self.power[::-1]):
             index = len(self.power) - index - 1
 
-            if self.power == [0]:
+            if set(self.power) == {0}:
                 return '0'
             if num == 0:
                 continue
+
             if index == 0:
-                string += "{}".format(num)
+                string += "{:+}".format(num)
                 break
             else:
                 part = "x"
-                if num != 1:
-                    part = "{}*".format(num) + part
+                if num == 1:
+                    part = '+' + part
+                elif num == -1:
+                    part = '-' + part
+                else:
+                    part = "{:+}*".format(num) + part
+
                 if index != 1:
                     part += "**{}".format(index)
+            string += part
 
-                string += part
-
-            string += '+'
-        if string.endswith('+'):
-            return string[:-1]
-        return string
+        return string.strip('+')
 
 
-x = X()
-while True:
-    inp = input()
-    print(eval(inp))
+# x = X()
+# while True:
+#     inp = input()
+#     print(eval(inp))
 
 
 class ExpressionEvaluator:
@@ -109,8 +124,6 @@ class ExpressionEvaluator:
         self.tokens = None
         self.tok = None
         self.nexttok = None
-        self.power = [0]
-        self.stack = [[]]
 
     # Parser
     def parse(self, text):
@@ -118,8 +131,8 @@ class ExpressionEvaluator:
         self.tok = None
         self.nexttok = None
         self._advance()
-        print(self.expr())
-        # return self.expr()
+        # print(self.expr())
+        return str(self.expr())
 
     def _advance(self):
         self.tok, self.nexttok = self.nexttok, next(self.tokens, None)
@@ -139,31 +152,31 @@ class ExpressionEvaluator:
     def expr(self):
         """term (+|-) term"""
         exprval = self.term()
-        if exprval == 'x':
-            self.stack[-1].append('x')
+        # if exprval == 'x':
+        #     self.stack[-1].append('x')
         while self._accept('PLUS') or self._accept('MINUS'):
             op = self.tok.type
             right = self.term()
 
             if op == 'PLUS':
                 exprval += right
+                # exprval = ('+', exprval, right)
             elif op == 'MINUS':
                 exprval -= right
+                # exprval = ('-', exprval, right)
         return exprval
 
     def term(self):
         """factor (*) factor"""
         termval = self.factor()
-        if termval == 'x':
-            self.stack[-1].append('x')
+
         while self._accept('TIMES'):
             op = self.tok.type
             right = self.factor()
 
             if op == 'TIMES':
                 termval *= right
-                # elif op == 'POWER':
-                #     termval = math.pow(termval, right)
+                # termval = ('*', termval, right)
         return termval
 
     def factor(self):
@@ -171,9 +184,9 @@ class ExpressionEvaluator:
         if self._accept('NUM'):
             return int(self.tok.value)
         elif self._accept('X'):
-            return self.tok.value
+            # return self.tok.value
+            return X()
         elif self._accept('LPAREN'):
-            self.stack.append([])
             exprval = self.expr()
             self._expect('RPAREN')
             return exprval
@@ -183,13 +196,23 @@ class ExpressionEvaluator:
 
 def simplify(data):
     e = ExpressionEvaluator()
+    print(data, e.parse(data))
     print(e.parse(data))
+    return e.parse(data)
 
-# simplify("x+x*x+x*x*x") == "x**3+x**2+x"
-# simplify("(x-1)*(x+1)") == "x**2-1"
-# simplify("(x+1)*(x+1)") == "x**2+2*x+1"
-# simplify("(x+3)*x*2-x*x") == "x**2+6*x"
-# simplify("(2*x+3)*2-x+x*x*x*x") == "x**4+3*x+6"
-# simplify("x*x-(x-1)*(x+1)-1") == "0"
-# simplify("5-5-x") == "-x"
-# simplify("x*x*x-x*x*x-1") == "-1"
+
+# while True:
+#     inp = input()
+#     simplify(inp)
+
+simplify(u"(x+13-(x*x*x)*x-(x)-x+19-x*10*(x*62)+x-x*0*(66)*83-93+x-(21*x+(85-77+x-x)))")
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert simplify("(x-1)*(x+1)") == "x**2-1", "First and simple"
+    assert simplify("(x+1)*(x+1)") == "x**2+2*x+1", "Almost the same"
+    assert simplify("(x+3)*x*2-x*x") == "x**2+6*x", "Different operations"
+    assert simplify("x+x*x+x*x*x") == "x**3+x**2+x", "Don't forget about order"
+    assert simplify("(2*x+3)*2-x+x*x*x*x") == "x**4+3*x+6", "All together"
+    assert simplify("x*x-(x-1)*(x+1)-1") == "0", "Zero"
+    assert simplify("5-5-x") == "-x", "Negative C1"
+    assert simplify("x*x*x-x*x*x-1") == "-1", "Negative C0"
