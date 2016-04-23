20514d690214acd7a53957f3661dc789360c96b1
enihsyou <s1131234@gmail.com>
2016-04-23 17:58:36 +0800

    #11 Space Golf

--- Space Golf.py
+++ Space Golf.py
@@ -1,15 +1,12 @@
-import math
+from math import hypot as y
 import itertools as o
-
-a = lambda a, i: math.hypot(a[0] - i[0], a[1] - i[1])
 
 
 def golf(t):
-    q = 99
-    for u in o.permutations(t):
-        b = sum(a(*c) for c in list(zip(u, u[1:]))) + a((0, 0), u[0])
-        if b < q: q = b
-    return q
+    return min([sum(y(i[0] - j[0], i[1] - j[1]) for i, j in list(zip(u, u[1:]))) + y(u[0][0], u[0][1]) for u in o.permutations(t)])
+
 
 
 print(golf({(2, 2), (2, 8), (8, 8), (8, 2), (5, 5)}))
+
+
