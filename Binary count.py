bbe9c7e80cc1724617f7ee3cf5fa478899ce6dc0
enihsyou <s1131234@gmail.com>
2016-05-05 22:38:59 +0800

    CheckiO/elementary/Binary count

--- Binary count.py
+++ Binary count.py
@@ -4,3 +4,15 @@ Reference: https://checkio.org/mission/binary-count/
 Time: 2016-05-05
 By: enihsyou
 """
+
+
+def checkio(number):
+    return bin(number).count('1')
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(4) == 1
+    assert checkio(15) == 4
+    assert checkio(1) == 1
+    assert checkio(1022) == 9
