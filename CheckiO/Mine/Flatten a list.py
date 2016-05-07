32b632f2e59af3867dacba0530a0e221d99409aa
enihsyou <s1131234@gmail.com>
2016-05-07 16:33:18 +0800

    CheckiO/Mine/Flatten a list 神奇的做法 增加一个别人的做法

--- CheckiO/Mine/Flatten a list.py
+++ CheckiO/Mine/Flatten a list.py
@@ -5,8 +5,7 @@ Time: 2016-05-07
 By: enihsyou
 """
 
-# c = lambda x: x
-# flat_list = lambda x: [a if isinstance(a, int) else c(a) for a in x]
 flat_list = lambda x: eval('[' + str(x).replace('[', '').replace(']', '') + ']')
+# flat_list = lambda L: sum(map(flat_list, L), []) if isinstance(L, list) else [L]  # 别人的做法
 print(flat_list([1, [2, 2, 2], 4]))
 print(flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]))
