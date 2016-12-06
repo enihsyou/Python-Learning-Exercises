778c0810d094804f8ece5a3ea19640266cd0badf
九条涼果 <s1131234@gmail.com>
2016-12-06 21:59:43 +0800

    161206

--- 161129第七次Python/future_year.py
+++ 161129第七次Python/future_year.py
@@ -10,7 +10,24 @@ day_to_chinese = {
     5: "六",
     6: "日",
 }
-this_year = datetime.datetime.now().year
-for year in range(this_year, this_year + 100):
-    birthday = datetime.date(year, 3, 9)
-    print("{}年的生日是星期{}".format(year, day_to_chinese[birthday.weekday()]))
+format_pattern = "%Y-%m-%d"
+
+if __name__ == '__main__':
+    input_date = input("输入年-月-日(2016-12-06)：")
+    try:
+        input_date_parsed = datetime.datetime.strptime(
+            input_date if input_date.strip()
+            else datetime.date.today().strftime(format_pattern),
+            format_pattern)
+    except ValueError:
+        print("输入格式有误")
+    else:
+        print("{}是星期{}".format(
+            input_date_parsed.strftime(format_pattern),
+            day_to_chinese[input_date_parsed.weekday()]))
+    finally:
+        this_year = datetime.datetime.now().year
+        for year in range(this_year, this_year + 100):
+            birthday = datetime.date(year, 3, 9)
+            print("{}年的生日是星期{}".format(
+                year, day_to_chinese[birthday.weekday()]))
--- 161129第七次Python/is_leap_year.py
+++ 161129第七次Python/is_leap_year.py
@@ -6,6 +6,8 @@ def is_leap_year(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
-print("本世纪有", sum(((print(year) or 1)
-                   if is_leap_year(year) else 0
-                   for year in range(2000, 2099))), "个闰年")
+if __name__ == '__main__':
+    print("是闰年" if is_leap_year(int(input("输入年份："))) else "不是闰年")
+    print("\n本世纪有", sum(((print(year, end=' ') or 1)
+                         if is_leap_year(year) else 0
+                         for year in range(2000, 2099))), "个闰年")
